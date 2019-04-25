650884a87c560702c58ac1cf3470afa09eaffcc7
BoyStot <29760967+BoyStot@users.noreply.github.com>
2019-04-25 18:48:29 +0100

    Faster still
    Get more than the max score with a little tweaking.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+$row1.html('Start'); // Clear out current spans.
+row1_string = ''; // clear string so the spans dont get added again with same numbers
+word_string = $('#wordlist').text(); // grab the word list
+
+word_string = word_string+word_string; // 2
+word_string = word_string+word_string; // 4
+word_string = word_string+word_string; // 8
+word_string = word_string+word_string; // 16
+word_string = word_string+word_string; // multiply word list out 32 times
+
+$("#wordlist").text(word_string); // reset wordlist element to new word_string for stats calculation by server.
+words = word_string.split("|");
+				
+fill_line_switcher(); // re-poulate words div.
+
+p = $('#row1 span[wordnr="'+word_pointer+'"]').position();
+
 var input = $('#inputfield')[0]
 var ev = $.Event('keyup')
 ev.which = 32
@@ -7,4 +24,4 @@ setInterval(function() {
         input.value = $('.highlight').text()
         $(input).trigger(ev)
     }
-}, 100)
+}, 1)
